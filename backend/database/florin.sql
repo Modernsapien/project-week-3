26c10dbe0256a1ce05c9fa8a6d40ba13a8ac9be3
anthonychan1211 <anthonychan1211@gmail.com>
2023-07-21 14:38:24 +0100

    email key removed

--- backend/database/florin.sql
+++ backend/database/florin.sql
@@ -12,7 +12,7 @@ CREATE TABLE users (
     email VARCHAR (255) UNIQUE NOT NULL,
     username VARCHAR(50) UNIQUE NOT NULL,
     password VARCHAR(255) NOT NULL,
-    is_verified BOOLEAN DEFAULT true
+    is_verified BOOLEAN DEFAULT false
 );
 
 
