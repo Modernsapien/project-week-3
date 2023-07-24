2c1d7e366ca5cb60e861d66cbb742673bd344969
mahirjali <mahirjalil@hotmail.com>
2023-07-24 11:45:43 +0100

    Created Login page

--- client/src/pages/Login/index.jsx
+++ client/src/pages/Login/index.jsx
@@ -1,5 +1,29 @@
+import { LoginForm } from "../../components";
+
 const Login = () => {
-  return <div>Login</div>;
+
+  return (
+    <>
+        <div className="wrapper">
+            <div className=" container main">
+                <div className="row">
+                    <div className="col-md-6 side-image">
+                        <img src="src/assets/logo.png" alt=""/>
+                    </div>
+                    <div className="col-md-6 right">
+                        <div className="small-logo">
+                            <img src="src/assets/logo.png" alt=""/>
+                        </div>
+                        <div className="input-box">
+                            <header>Welcome to Study Wise</header>
+                            <LoginForm />
+                        </div>  
+                    </div>
+                </div>
+            </div>
+        </div>
+      </>
+  )
 };
 
 export default Login;
