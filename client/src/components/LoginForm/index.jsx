6c7ed056288f0d68abeeaffa51ad0fe402783836
YF Chau <yfchauhk@gmail.com>
2023-07-25 16:56:48 +0100

    Login function completed.

--- client/src/components/LoginForm/index.jsx
+++ client/src/components/LoginForm/index.jsx
@@ -1,17 +1,48 @@
 import "./styles.css";
 import Form from 'react-bootstrap/Form';
 import FloatingLabel from 'react-bootstrap/FloatingLabel';
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useState } from "react"
+import { useAuth } from "../../contexts"
 
 export default function LoginForm() {
+
+    const [form, setForm] = useState({ username: "", password:"" });
+    const { user, setUser } = useAuth();
+    const navigate = useNavigate();
+
+    async function handleSubmit(e) {
+        e.preventDefault();
+        const options = {
+            method: "POST",
+            headers: {"content-type": "application/json"},
+            body: JSON.stringify(form)
+        }
+        const res = await fetch('http://localhost:3000/user/login', options)
+        if (res.ok) {
+            const data = await res.json()
+            if (data.authenticated) {
+                // set AuthContext to username
+                setUser({ 
+                    firstName: data.user.firstName,
+                    lastName: data.user.lastName,
+                    userId: data.user.id
+                 })
+                // set token to local storage
+                localStorage.setItem("token", data.token)
+                navigate("/")
+            }
+        }
+    }
+
   return (
       <>
-        <form>
+        <form onSubmit={handleSubmit}>
             <FloatingLabel controlId="floatingInput" label="Username" className="mb-3">
-                <Form.Control type="text" placeholder="username" />
+                <Form.Control onChange={(e) => setForm({...form, username: e.target.value})} type="text" placeholder="username" />
             </FloatingLabel>
             <FloatingLabel controlId="floatingPassword" label="Password">
-                <Form.Control type="password" placeholder="Password" />
+                <Form.Control onChange={(e) => setForm({...form, password: e.target.value})}type="password" placeholder="Password" />
             </FloatingLabel>
             <div className="input-field">
                 <input type="submit" className="submit" value="Login" />
