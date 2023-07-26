01093f2e959a165d3042de255e48a626633bf2d8
YF Chau <yfchauhk@gmail.com>
2023-07-26 15:49:10 +0100

    Merge

--- backend/database/studywise.sql
+++ backend/database/studywise.sql
@@ -61,9 +61,3 @@ INSERT INTO todos (todo_title, todo_description, is_finished, user_id) VALUES
     ('Buy groceries', 'Lots of stuff to buy', false, 1),
     ('Finish report', 'Pull a all-nighter', false, 1),
     ('Call mom', 'To say happy birthday', false, 1);
-
-
-
-
-    (8, 'Michael', 'Lee', 'yfchauhk@gmail.com', 'mlee', '$2b$10$WxMO5IfOSvvsNi4rDsPc9uEH.I4y1MuGj6W8.sFyTEq48.maIVuLu', true),
-    (1, 'Anthony', 'Chan', 'anthonychan1211@gmail.com', 'anthony', '$2b$10$ESylvA.25PVWUQQk/jLfd.FHiju/U.mxb4pnKxevyY0OYtj8dO3a6', true);
