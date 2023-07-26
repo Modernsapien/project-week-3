c2c4edca263902abba29afce6abdcc9fdc31bcee
ioptyu2 <mbatta1996@hotmail.com>
2023-07-26 13:15:09 +0100

    created initial token testing

--- backend/__tests__/integration/token.spec.js
+++ backend/__tests__/integration/token.spec.js
@@ -4,6 +4,9 @@ const db = require("../../database/db")
 
 describe("Calendar", () => {
     let api
+    let token
+    let token_id
+    let user_id
 
     beforeAll(() => {
         api = app.listen(5000, () => {
@@ -17,6 +20,59 @@ describe("Calendar", () => {
         api.close(done)
     })
 
+    //CREATE TOKEN
+    it("should create a token", async () => {
+        //CREATE TEST USER
+        const testUser = {
+            first_name: "testF",
+            last_name: "testL",
+            userEmail: "test@test.com",
+            username: "testU",
+            password: "testP"
+        }
+        let response = await request(app)
+                .post("/user/register")
+                .send(data)
+
+        user_id = response.body.user_id
+
+        response = await request(app)
+            .post(`/token/create/${user_id}`)
+            .expect(200)
+
+        token = response.body.token
+        token_id = response.body.token_id
+
+    })
+
+    //GET ONE BY ID
+    it("should return token with provided token_id", async () => {
+        const response = await request(app)
+            .get(`/token/user${token_id}`)
+            .expect(200)
+
+        expect(response.body.token).toEqual(token)
+    })
+
     //GET ONE BY TOKEN
-    // it("should return a token ")
+    it("should return token with provided token", async () => {
+        const response = await request(app)
+            .get(`/token/token/${token}`)
+            .expect(200)
+
+        expect(response.body.token_id).toEqual(token_id)
+    })
+
+    //DELETE TOKEN
+    it("should delete token with provided token", async () => {
+        let response = await request(app)
+            .delete(`/token`)
+            .send(data = {
+                token
+            })
+            .expect(204)
+
+        response = await request(app)
+            .delete(`/user/${user_id}`)
+    })
 })
